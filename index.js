27e377de9688d5fbfe8ec32eb7cc93b72675bf26
Prajeet <prajeetsinghtomar@gmail.com>
2022-10-27 17:34:27 +0530

    Inital commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var numberOfitems = document.querySelectorAll(".drum").length;
+for(var i = 0; i<numberOfitems;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    
+    var buttoninnerHTML = this.innerHTML;
+    makeSound(buttoninnerHTML)
+    buttonAnimation(buttoninnerHTML)
+    });
+}
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key)
+    buttonAnimation(event.key)
+});
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+         case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+       
+        default: console.log(key);
+            
+    }
+    
+}
+function buttonAnimation(currentkey){
+    var activeButton = document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100)
+}
+
